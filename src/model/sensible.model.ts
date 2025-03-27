062513e6d99441afb10a56cd1b3d69d584b0aef9
Norman <norman.tetzlaff@gmail.com>
2025-03-27 15:39:03 +0100

    entwurf der User Schema erstellt

---
+++ src/model/sensible.model.ts
@@ -0,0 +1,144 @@
+import { Schema, model } from "mongoose";
+import crypto from "crypto-js";
+import "dotenv/config.js";
+
+const encriptionKey = process.env.ENCRYPTION_KEY;
+
+const encryptData = (data: string) => {
+  const cipher = crypto.AES.encrypt(data, encriptionKey as string).toString();
+  return cipher;
+};
+
+const decryptData = (data: string) => {
+  const bytes = crypto.AES.decrypt(data, encriptionKey as string);
+  const decryptedData = bytes.toString(crypto.enc.Utf8);
+  return decryptedData;
+};
+
+const onlyLetters = /^[A-Za-zÄÖÜäöüßÉéÈèȨȩÑñÇç\- ]+$/;
+const telNumber = /^[0-9+ ]+$/;
+const email = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const iban = /^[A-Z]{2}[0-9]{2}[a-zA-Z0-9]{4}[0-9]{7}([a-zA-Z0-9]?){0,16}$/;
+
+const addressSchema = new Schema({
+  street: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 3,
+    maxLength: 50,
+    validate: {
+      validator: function (val: string) {
+        return onlyLetters.test(val);
+      },
+      message: "Street must contain only letters",
+    },
+  },
+  houseNumber: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 1,
+    maxLength: 4,
+  },
+  city: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 3,
+    maxLength: 50,
+    validate: {
+      validator: function (val: string) {
+        return onlyLetters.test(val);
+      },
+      message: "City must contain only letters",
+    },
+  },
+  postalCode: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 4,
+    maxLength: 5,
+  },
+});
+
+const sensibleDataSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  address: {
+    type: addressSchema,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 10,
+    maxLength: 15,
+    set: (val: string) => encryptData(val),
+    get: (val: string) => decryptData(val),
+  },
+  paymentMethods: [
+    {
+      methode: {
+        type: String,
+        enum: ["creditCard", "paypal", "bankTransfer"],
+        required: true,
+      },
+      creditCard: {
+        cardToken: {
+          type: String,
+          set: (val: string) => encryptData(val),
+          get: (val: string) => decryptData(val),
+        },
+        expirationDate: Date,
+        cardType: String,
+      },
+      paypal: {
+        type: String,
+        validate: {
+          validator: function (val: string) {
+            return email.test(val);
+          },
+          message: "Invalid email",
+        },
+      },
+      bankTransfer: {
+        bankAccountNumber: Number,
+        bankName: String,
+        iban: {
+          type: String,
+          validate: {
+            validator: function (val: string) {
+              return iban.test(val);
+            },
+            message: "Invalid iban",
+          },
+          set: (val: string) => encryptData(val),
+          get: (val: string) => decryptData(val),
+          bic: String,
+        },
+      },
+      isPrimary: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  ],
+  shippingAddress: {
+    type: addressSchema,
+    required: true,
+  },
+  billingAddress: {
+    type: addressSchema,
+    required: true,
+  },
+});
+
+const SensibleData = model("SensibleData", sensibleDataSchema);
+
+export default SensibleData;
