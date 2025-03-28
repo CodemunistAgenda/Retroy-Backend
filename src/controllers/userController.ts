19751ce674f0ebbc07d4c0766983aa30799d4213
Norman <norman.tetzlaff@gmail.com>
2025-03-29 00:56:38 +0100

    added the register route
    that includes the verifying Routes

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -7,6 +7,7 @@ import zxcvbn from "zxcvbn";
 
 import User from "../model/user.model.ts";
 import { blacklistedMails } from "../utils/tempmailing.ts";
+import { sendVerficationEmail } from "../middleware/sendingMails.ts";
 
 const registerLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
@@ -14,64 +15,56 @@ const registerLimiter = rateLimit({
   message: "Too many requests, please try again later.",
 });
 
-export const register = async (req: Request, res: Response) => {
+export const register = async (req: Request, res: Response): Promise<void> => {
   const { Firstname, secondName, Lastname, email, password, username } = req.body;
 
   try {
     if (!Firstname || !Lastname || !email || !password || !username) {
-      return res.status(400).json({ message: "Please fill all fields" });
+      res.status(400).json({ message: "Please fill all fields" });
     }
 
     if (!Validator.isEmail(email)) {
-      return res.status(400).json({ message: "Invalid email" });
+      res.status(400).json({ message: "Invalid email" });
     }
 
     const mailDomain = email.split("@")[1].toLowerCase();
     if (blacklistedMails.includes(mailDomain)) {
-      return res.status(400).json({ message: "Temporary email addresses are not allowed" });
+      res.status(400).json({ message: "Temporary email addresses are not allowed" });
     }
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: "this mail is in use" });
+      res.status(400).json({ message: "this mail is in use" });
     }
 
     const passwordStrength = zxcvbn(password);
     if (passwordStrength.score < 3) {
-      return res.status(400).json({
+      res.status(400).json({
         message: passwordStrength.feedback.suggestions.join(" "),
       });
     }
 
     const hashedPassword = await bcrypt.hash(password, 12);
-    let newUser;
-
-    if (secondName) {
-      newUser = new User({
-        firstName: Firstname,
-        secondName: secondName,
-        lastName: Lastname,
-        email: email,
-        password: hashedPassword,
-        userName: username,
-      });
-    } else {
-      newUser = new User({
-        firstName: Firstname,
-        lastName: Lastname,
-        email: email,
-        password: hashedPassword,
-        userName: username,
-      });
+    if (!hashedPassword) {
+      res.status(500).json({ message: "Error hashing password" });
+      return;
     }
 
+    const newUser = new User({
+      firstName: Firstname,
+      secondName,
+      lastName: Lastname,
+      email,
+      password: hashedPassword,
+      userName: username,
+    });
     await newUser.save();
 
     const verificationToken = jwt.sign({ email }, process.env.JWT_SECRET as string, {
       expiresIn: "1h",
     });
 
-    console.log(`Verifying link: http://fakelink.com/verify?token=${verificationToken}`);
+    await sendVerficationEmail(email, verificationToken);
 
     res.status(201).json({
       message: "User registered successfully",
@@ -82,4 +75,4 @@ export const register = async (req: Request, res: Response) => {
   }
 };
 
-export const login = async (req: Request, res: Response) => {};
+export const login = async (req: Request, res: Response): Promise<void> => {};
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,9 +1,10 @@
-import { register, login } from "../controllers/userController.ts";
 import express, { Router } from "express";
+
+import { register, login } from "../controllers/userController.ts";
+import { verifyUser } from "../controllers/verifyUser.ts";
 
 const userRouter = Router();
 
-userRouter.post("/register", register);
-userRouter.post("/login", login);
+userRouter.post("/register", register).post("/login", login).get("/verify", verifyUser);
 
 export default userRouter;
